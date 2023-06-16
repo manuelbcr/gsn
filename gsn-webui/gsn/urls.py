a0d4d7f377b48a83f310f26da19e223d438cde62
Davide De Sclavis <davide.de-sclavis@student.uibk.ac.at>
2023-06-16 14:20:46 +0200

    fix[gsn-webui]: fix urls

--- gsn-webui/gsn/urls.py
+++ gsn-webui/gsn/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     re_path(r'^download/$', csrf_exempt(views.download), name='download'),
     re_path(r'^profile/$', views.profile, name='profile'),
     re_path(r'^logout/$', views.logout_view, name='logout'),
-    re_path(r'^admin/', include(admin.site.urls)),
+    re_path(r'^admin/', admin.site.urls),
     re_path(r'^oauth_code/$', views.oauth_get_code, name='oauth_logging_redirect'),
     re_path(r'^favorites/$', views.favorites_manage, name='favorites'),
     re_path(r'^favorites_list/$', views.favorites_list, name='favorites_list'),
