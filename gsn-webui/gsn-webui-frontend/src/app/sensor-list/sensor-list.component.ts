6bc099f66e741eff2d81de1dd610e82d9e1eb417
Davide De Sclavis <davide.de-sclavis@student.uibk.ac.at>
2023-06-23 15:07:30 +0200

    add[gsn-webui-frontend]: add OpenLayers (https://openlayers.org/) map to sensorListComponent

--- gsn-webui/gsn-webui-frontend/src/app/sensor-list/sensor-list.component.ts
+++ gsn-webui/gsn-webui-frontend/src/app/sensor-list/sensor-list.component.ts
@@ -1,5 +1,30 @@
+/*
+ * sensor-list.component.ts
+ * Component for sensor list
+ *
+ * This file contains code based on or derived from OpenLayers (https://openlayers.org/),
+ * which is licensed under the BSD 2-Clause License.
+ *
+ * Copyright (c) 2005-present, OpenLayers Contributors All rights reserved.
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
 import { Component, OnInit } from '@angular/core';
-import { SensorService } from '../services/sensor.service';
+import { SensorService } from "src/app/services/sensor.service";
+
+// Add the OpenLayers imports
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import XYZ from 'ol/source/XYZ';
+import Feature from 'ol/Feature';
+import Point from 'ol/geom/Point';
+import { fromLonLat } from 'ol/proj';
+import { Style, Icon } from 'ol/style';
+import { Vector } from 'ol/layer';
+import { Vector as VectorSource } from 'ol/source';
 
 
 @Component({
@@ -9,27 +34,92 @@ import { SensorService } from '../services/sensor.service';
 })
 export class SensorListComponent implements OnInit {
   loading = true;
-  sensors: any[] =[];
-  favorites: any[] =[];
+  sensors: any[] = [];
+  favorites: any[] = [];
+  lat = 0; // Set the initial latitude
+  lng = 0; // Set the initial longitude
+  map: any; // Declare a map variable
 
 
   constructor(
     private sensorService: SensorService) {
 
-    }
+  }
 
   ngOnInit() {
     this.loadSensors();
   }
 
   loadSensors() {
-    this.sensorService.getSensors().subscribe((data: any) => {
-      this.sensors = data.features;
-      this.loading = false;
+    this.sensorService.getSensors().subscribe(
+      (data: any) => {
+        this.sensors = data.features;
+        this.loading = false;
+        console.log(this.sensors);
+        // Set the center of the map to the first sensor's coordinates
+        if (this.sensors.length > 0) {
+          const firstSensor = this.sensors[0];
+          this.lat = firstSensor.geometry.coordinates[0];
+          this.lng = firstSensor.geometry.coordinates[1];
+          this.initMap(); // Initialize the map
+        }
+      },
+      (error: any) => {
+        console.error(error);
+      }
+    );
+  }
 
-    }, (error: any) => {
-      console.error(error);
+
+  initMap() {
+    // Create a new map instance
+    this.map = new Map({
+      target: 'map', // Set the target HTML element where the map will be rendered
+      layers: [
+        new TileLayer({
+          source: new XYZ({
+            url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png' // Use OpenStreetMap as the base layer
+          })
+        })
+      ],
+      view: new View({
+        center: [this.lng, this.lat], // Set the initial center of the map
+        zoom: 10 // Set the initial zoom level
+      })
+    });
+    // Create a vector source and layer for markers
+    const markerSource = new VectorSource();
+    const markerLayer = new Vector({
+      source: markerSource
+    });
+
+    // Add the marker layer to the map
+    this.map.addLayer(markerLayer);
+
+    // Set the center of the map to the first sensor's coordinates
+    const firstSensor = this.sensors[0];
+    const firstSensorCoordinates = firstSensor.geometry.coordinates;
+    const centerCoordinates = fromLonLat([firstSensorCoordinates[0], firstSensorCoordinates[1]]);
+    this.map.getView().setCenter(centerCoordinates);
+
+    // Loop through the sensors and add markers to the map
+    this.sensors.forEach(sensor => {
+      const coordinates = sensor.geometry.coordinates;
+      const marker = new Feature({
+        geometry: new Point(fromLonLat(coordinates))
+      });
+
+      const markerStyle = new Style({
+        image: new Icon({
+          src: '../../assets/285659_marker_map_icon.svg', // Provide the path to your marker icon
+          anchor: [0.5, 1] // Set the anchor point of the icon (adjust if needed)
+        })
+      });
+
+      marker.setStyle(markerStyle);
+      markerSource.addFeature(marker);
     });
   }
+
 
 }
