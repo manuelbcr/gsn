d6ae5898626bdda193d81033b7b4b26762c2e6c7
Davide De Sclavis <davide.de-sclavis@student.uibk.ac.at>
2023-10-21 13:43:40 +0200

    mod[gsn-services]: update gsn-services according to new play 2.6

--- gsn-services/app/service/gsn/MyUserService.java
+++ gsn-services/app/service/gsn/MyUserService.java
@@ -1,42 +1,21 @@
-/**
-* Global Sensor Networks (GSN) Source Code
-* Copyright (c) 2006-2016, Ecole Polytechnique Federale de Lausanne (EPFL)
-* 
-* This file is part of GSN.
-* 
-* GSN is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-* 
-* GSN is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-* 
-* You should have received a copy of the GNU General Public License
-* along with GSN.  If not, see <http://www.gnu.org/licenses/>.
-* 
-* File: app/service/gsn/GSNUSerServicePlugin.java
-*
-* @author Julien Eberle
-*
-*/
 package service.gsn;
 
-import models.gsn.auth.User;
-import play.Application;
-
+import com.feth.play.module.pa.PlayAuthenticate;
+import com.feth.play.module.pa.service.AbstractUserService;
 import com.feth.play.module.pa.user.AuthUser;
 import com.feth.play.module.pa.user.AuthUserIdentity;
-import com.feth.play.module.pa.service.UserServicePlugin;
-import com.google.inject.Inject;
+import models.gsn.User;
 
-public class GSNUserServicePlugin extends UserServicePlugin {
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+
+@Singleton
+public class MyUserService extends AbstractUserService {
 
 	@Inject
-	public GSNUserServicePlugin(final Application app) {
-		super(app);
+	public MyUserService(final PlayAuthenticate auth) {
+		super(auth);
 	}
 
 	@Override
