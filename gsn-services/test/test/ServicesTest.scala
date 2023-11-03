752379b314cbd7cd5eaad6e1646460803f797f15
Davide De Sclavis <davide.de-sclavis@student.uibk.ac.at>
2023-11-03 12:12:29 +0100

    mod[gsn-services]: add tests

--- gsn-services/test/test/ServicesTest.scala
+++ gsn-services/test/test/ServicesTest.scala
@@ -16,9 +16,12 @@ import play.api.libs.ws.ahc.AhcWSClient
 import play.api.libs.json.Json
 import scala.concurrent.Await
 import scala.concurrent.duration._
+import play.api.libs.json._
+import org.scalatest.BeforeAndAfter
 
 //include multiformatsample.xml and start core if not executable
-class ServicesTest extends PlaySpec {
+//important also add the user to virtual sensor in frontend in order to access the data 
+class ServicesTest extends PlaySpec with BeforeAndAfter{
 
   val actorSystem = ActorSystem("test")
   implicit val ec: ExecutionContext = scala.concurrent.ExecutionContext.Implicits.global
@@ -28,6 +31,33 @@ class ServicesTest extends PlaySpec {
       bind[ControllerComponents].to[DefaultControllerComponents]
     )
     .build()
+
+
+
+  var access_token: String = ""
+
+  before {
+    // Run once before all tests to obtain the access token
+    val oauthcontroller = app.injector.instanceOf[OAuth2Controller]
+    val oauth2request = FakeRequest("POST", "/oauth2/token")
+    val requestData = Map(
+      "grant_type" -> "client_credentials",
+      "client_id" -> "web-gui-public",
+      "client_secret" -> "web-gui-secret"
+    )
+
+    val futureAccessToken = oauthcontroller.accessToken()(oauth2request.withFormUrlEncodedBody(requestData.toSeq: _*))
+
+    val tokenresult: Result = await(futureAccessToken)
+    println(tokenresult)
+
+    if (tokenresult.header.status == OK) {
+      val json = Json.parse(contentAsString(futureAccessToken))
+      access_token = (json \ "access_token").as[String]
+    } else {
+      throw new RuntimeException(s"Access token request failed with status: ${tokenresult.header.status}")
+    }
+  }
 
   "DataProcessService" should {
     "processData method should return JSON response" in {
@@ -82,9 +112,8 @@ class ServicesTest extends PlaySpec {
 
   "SensorService" should {
 
-
-    //sensors
-    "return sensor data in JSON format" in {
+    //##################################################### sensors #####################################################
+    "return sensors in JSON format" in {
       val sensorService = app.injector.instanceOf[SensorService]
       val request = FakeRequest("GET", "/api/sensors?format=json")
       val result = sensorService.sensors()(request)
@@ -93,7 +122,7 @@ class ServicesTest extends PlaySpec {
       contentType(result) mustBe Some("application/json")
     }
 
-    "return sensor data in CSV format" in {
+    "return sensors in CSV format" in {
       val sensorService = app.injector.instanceOf[SensorService]
       val request = FakeRequest("GET", "/api/sensors?format=csv")
       val result = sensorService.sensors()(request)
@@ -102,7 +131,7 @@ class ServicesTest extends PlaySpec {
       contentType(result) mustBe Some("text/plain")
     }
 
-    "return sensor data in XML format" in {
+    "return sensors in XML format" in {
       val sensorService = app.injector.instanceOf[SensorService]
       val request = FakeRequest("GET", "/api/sensors?format=xml")
       val result = sensorService.sensors()(request)
@@ -111,7 +140,7 @@ class ServicesTest extends PlaySpec {
       contentType(result) mustBe Some("application/xml")
     }
 
-    "return sensor data in default response format (JSON)" in {
+    "return sensors in default response format (JSON)" in {
       val sensorService = app.injector.instanceOf[SensorService]
       val request = FakeRequest("GET", "/api/sensors")
       val result = sensorService.sensors()(request)
@@ -121,25 +150,8 @@ class ServicesTest extends PlaySpec {
     }
 
 
-    //userinfo
-    "return user info in JSON format" in {
-      val oauthcontroller = app.injector.instanceOf[OAuth2Controller]
-      val oauth2request = FakeRequest("POST", "/oauth2/token")
-      val requestData = Map(
-        "grant_type" -> "client_credentials",
-        "client_id" -> "web-gui-public",
-        "client_secret" -> "web-gui-secret"
-      )
-
-      val futureAccessToken = oauthcontroller.accessToken()(oauth2request.withFormUrlEncodedBody(requestData.toSeq: _*))
-
-      val tokenresult: Result = Await.result(futureAccessToken, 10.seconds)
-
-      // Check if the response status is 200 OK
-      if (tokenresult.header.status == OK) {
-        // Parse the JSON content to extract the access token
-        val json = Json.parse(contentAsString(futureAccessToken))
-        val access_token = (json \ "access_token").as[String]
+    //##################################################### userinfo #####################################################
+    "return user info in JSON format for userinfo request" in {
         val request = FakeRequest("GET", "/api/user").withHeaders(
           "Authorization" -> s"Bearer $access_token"
         )
@@ -148,25 +160,65 @@ class ServicesTest extends PlaySpec {
 
         status(result) mustBe OK
         contentType(result) mustBe Some("application/json")
-      } else {
-        println(s"Access token request failed with status: ${tokenresult.header.status}")
-      }
+
     }
 
-
-
-
-/*    "return Forbidden for unauthenticated request" in {
-      // Create a FakeRequest without an OAuth token
+    "return BAD_REQUEST for userinfo request with no token" in {
       val request = FakeRequest("GET", "/api/user")
       val sensorService = app.injector.instanceOf[SensorService]
       val result = sensorService.userInfo()(request)
+      status(result) mustBe BAD_REQUEST
+    }
 
-      status(result) mustBe FORBIDDEN
-    }*/
+    "return unauthorized for userinfo request with wrong token" in {
+      val request = FakeRequest("GET", "/api/user").withHeaders(
+          "Authorization" -> s"Bearer fake_token"
+      )
+      val sensorService = app.injector.instanceOf[SensorService]
+      val result = sensorService.userInfo()(request)
+      status(result) mustBe UNAUTHORIZED
+    }
 
+    //##################################################### sensorData #####################################################
+      "return sensor data in JSON format with expectedFields and fieldTypes" in {
+        val params = Map("size" -> "10", "fields" -> "light,temperature,packet_type","filter"-> "light > 100,temperature <100")
 
+        val request = FakeRequest("GET", s"/api/sensors/MultiFormatTemperatureHandler/data?${params.map { case (key, value) => s"$key=$value" }.mkString("&")}")
+          .withHeaders("Authorization" -> s"Bearer $access_token")
+        println(request)
+        val sensorService = app.injector.instanceOf[SensorService]
+        val futureResult = sensorService.sensorData("MultiFormatTemperatureHandler")(request)
+        val result: Result = await(futureResult)
 
+        status(futureResult) mustBe OK
+        contentType(futureResult) mustBe Some("application/json")
+   
+        val jsonResult = Json.parse(contentAsString(futureResult))
+        println(jsonResult)
+
+        (jsonResult \ "type").as[String] mustBe "Feature"
+
+        val properties = (jsonResult \ "properties").as[JsObject]
+        (properties \ "vs_name").as[String] mustBe "MultiFormatTemperatureHandler"
+
+        val fields = (properties \ "fields").as[Seq[JsObject]]
+
+            // Define the expected fields and their types
+            val expectedFields = Map(
+              "light" -> "double",
+              "temperature" -> "double",
+              "packet_type" -> "double"
+            )
+
+            for ((fieldName, fieldType) <- expectedFields) {
+              val field = fields.find(field => (field \ "name").as[String] == fieldName)
+              field.isDefined mustBe true
+              val fieldJsType = (field.get \ "type").as[String]
+              fieldJsType mustBe fieldType
+            }
+     
+        
+      }
   }
 
 
