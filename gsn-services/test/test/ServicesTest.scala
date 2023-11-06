32554263e033cfd537128b90c0447f450964bda4
Davide De Sclavis <davide.de-sclavis@student.uibk.ac.at>
2023-11-06 07:47:12 +0100

    add[gsn-services]: add more tests to servicestest

--- gsn-services/test/test/ServicesTest.scala
+++ gsn-services/test/test/ServicesTest.scala
@@ -87,6 +87,15 @@ class ServicesTest extends PlaySpec with BeforeAndAfter{
       contentType(result) mustBe Some("text/plain")
     }
 
+    "return XML response for processData method with linear-interp" in {
+      val controller = app.injector.instanceOf[DataProcessService]
+      val request = FakeRequest("GET", "/api/data?sensorid=MultiFormatTemperatureHandler&fieldid=light&op=linear-interp&params=10&format=xml")
+      val result = controller.processData("MultiFormatTemperatureHandler", "light")(request)
+
+      status(result) mustBe OK
+      contentType(result) mustBe Some("text/plain")
+    }
+
     "processData method should use the default response format (JSON)" in {
         val controller = app.injector.instanceOf[DataProcessService]
         val request = FakeRequest("GET", "/api/data?sensorid=MultiFormatTemperatureHandler&fieldid=light&op=wma&params=10")
@@ -194,7 +203,6 @@ class ServicesTest extends PlaySpec with BeforeAndAfter{
         contentType(futureResult) mustBe Some("application/json")
    
         val jsonResult = Json.parse(contentAsString(futureResult))
-        println(jsonResult)
 
         (jsonResult \ "type").as[String] mustBe "Feature"
 
@@ -202,23 +210,132 @@ class ServicesTest extends PlaySpec with BeforeAndAfter{
         (properties \ "vs_name").as[String] mustBe "MultiFormatTemperatureHandler"
 
         val fields = (properties \ "fields").as[Seq[JsObject]]
-
-            // Define the expected fields and their types
-            val expectedFields = Map(
+        val expectedFields = Map(
               "light" -> "double",
               "temperature" -> "double",
               "packet_type" -> "double"
-            )
+        )
 
-            for ((fieldName, fieldType) <- expectedFields) {
-              val field = fields.find(field => (field \ "name").as[String] == fieldName)
-              field.isDefined mustBe true
-              val fieldJsType = (field.get \ "type").as[String]
-              fieldJsType mustBe fieldType
-            }
+        for ((fieldName, fieldType) <- expectedFields) {
+          val field = fields.find(field => (field \ "name").as[String] == fieldName)
+          field.isDefined mustBe true
+          val fieldJsType = (field.get \ "type").as[String]
+          fieldJsType mustBe fieldType
+        }
      
         
       }
+      //##################################################### sensorField #####################################################
+      "return sensor field data" in {
+        val sensorService = app.injector.instanceOf[SensorService]
+        val sensorId = "MultiFormatTemperatureHandler"
+        val fieldId = "light"
+        val request = FakeRequest("GET", s"/api/sensors/$sensorId/fields/$fieldId")
+          .withHeaders("Authorization" -> s"Bearer $access_token")
+        val result = sensorService.sensorField(sensorId, fieldId)(request)
+
+        status(result) mustBe OK
+        contentType(result) mustBe Some("text/plain")
+
+      }
+
+      //##################################################### sensorMetadata #####################################################
+      "return sensor metadata in the specified format (JSON)" in {
+        val sensorService = app.injector.instanceOf[SensorService]
+        val sensorId = "MultiFormatTemperatureHandler"
+        val latestValues = "true" 
+        val format = "json" 
+        val request = FakeRequest("GET", s"/api/sensors/$sensorId/metadata?latestValues=$latestValues&format=$format")
+          .withHeaders("Authorization" -> s"Bearer $access_token")
+        val result = sensorService.sensorMetadata(sensorId)(request)
+
+        status(result) mustBe OK
+        contentType(result) mustBe Some("application/json")
+
+      }
+
+      "return sensor metadata in the specified format (CSV)" in {
+        val sensorService = app.injector.instanceOf[SensorService]
+        val sensorId = "MultiFormatTemperatureHandler"
+        val latestValues = "true" 
+        val format = "csv" 
+        val request = FakeRequest("GET", s"/api/sensors/$sensorId/metadata?latestValues=$latestValues&format=$format")
+          .withHeaders("Authorization" -> s"Bearer $access_token")
+        val result = sensorService.sensorMetadata(sensorId)(request)
+
+        status(result) mustBe OK
+        contentType(result) mustBe Some("text/plain")
+
+      }
+
+      "return sensor metadata in the specified format (xml)" in {
+        val sensorService = app.injector.instanceOf[SensorService]
+        val sensorId = "MultiFormatTemperatureHandler"
+        val latestValues = "true" 
+        val format = "xml" 
+        val request = FakeRequest("GET", s"/api/sensors/$sensorId/metadata?latestValues=$latestValues&format=$format")
+          .withHeaders("Authorization" -> s"Bearer $access_token")
+        val result = sensorService.sensorMetadata(sensorId)(request)
+
+        status(result) mustBe OK
+        contentType(result) mustBe Some("text/plain")
+
+      }
+
+
+      //##################################################### sensorSearch #####################################################
+      "return sensor search results in the specified format" in {
+        val sensorService = app.injector.instanceOf[SensorService]
+        val vsnames = "MultiFormatTemperatureHandler" 
+        val size = 10 
+        val fields = "light,temperature" 
+        val format = "json" 
+        val request = FakeRequest("GET", s"/api/sensors/search?vsnames=$vsnames&size=$size&fields=$fields&format=$format")
+          .withHeaders("Authorization" -> s"Bearer $access_token")
+        val result = sensorService.sensorSearch(request)
+
+        status(result) mustBe OK
+        contentType(result) mustBe Some("application/json")
+       
+        val formatcsv = "csv" 
+        val requestcsv = FakeRequest("GET", s"/api/sensors/search?vsnames=$vsnames&size=$size&fields=$fields&format=$formatcsv")
+          .withHeaders("Authorization" -> s"Bearer $access_token")
+        val resultcsv = sensorService.sensorSearch(requestcsv)
+
+        status(resultcsv) mustBe OK
+        contentType(resultcsv) mustBe Some("application/zip")
+
+        val formatxml = "xml" 
+        val requestxml = FakeRequest("GET", s"/api/sensors/search?vsnames=$vsnames&size=$size&fields=$fields&format=$formatxml")
+          .withHeaders("Authorization" -> s"Bearer $access_token")
+        val resultxml = sensorService.sensorSearch(request)
+
+        status(resultxml) mustBe OK
+        contentType(resultxml) mustBe Some("application/json")
+      }
+
+      //##################################################### uploadSensorData #####################################################
+     
+     /*
+      "upload sensor data and forward it to GSN core" in {
+        val sensorService = app.injector.instanceOf[SensorService]
+        val sensorId = "MultiFormatTemperatureHandler"
+        val jsonData = Json.obj(
+          "light" -> 400,
+          "temperature" -> 42
+        ) 
+
+        val request = FakeRequest("POST", s"/api/sensors/$sensorId/data")
+          .withHeaders("Authorization" -> s"Bearer $access_token")
+          .withJsonBody(jsonData)
+        val futureResult= sensorService.uploadSensorData(sensorId)(request)
+        val result = await(futureResult) 
+        println(result)
+        //status(futureResult) mustBe OK
+        //contentType(futureResult) mustBe Some("application/json")
+        //contentAsString(futureResult) must include("success")
+      }
+      */
   }
 
 
